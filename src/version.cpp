15e87c92434f79dc33e3028854780c19b8ac3179
Bernd Stramm <bernd.stramm@gmail.com>
2011-05-21 15:31:15 -0400

    bump version

--- src/version.cpp
+++ src/version.cpp
@@ -28,7 +28,7 @@
  
 namespace deliberate {
 
-  QString ProgramVersion::VersionNumber ("0.2.7");
+  QString ProgramVersion::VersionNumber ("0.2.8");
 
   QString ProgramVersion::ProgramName("Carpo");
   QString ProgramVersion::copyright ("Copyright (C) 2011 Bernd Stramm");
